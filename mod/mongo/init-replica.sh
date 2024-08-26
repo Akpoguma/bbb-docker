fb9ac64ac9a2acbd31db702c73d1905560710132
Destiny Isaac Akpoguma <akpogumaisaacdestiny@gmail.com>
2024-08-26 09:03:19 +0000

    mongo update

--- mod/mongo/init-replica.sh
+++ mod/mongo/init-replica.sh
@@ -1,26 +1,25 @@
 #!/bin/sh
 set -e
 
-
 host=${HOSTNAME:-$(hostname -f)}
 
-# shut down again
-mongod --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --shutdown
-# restart again binding to 0.0.0.0 to allow a replset with 10.7.7.6
-mongod --oplogSize 8 --replSet rs0 --noauth \
-   --config /tmp/docker-entrypoint-temp-config.json \
-   --bind_ip 0.0.0.0 --port 27017 \
-   --tlsMode disabled \
-   --logpath /proc/1/fd/1 --logappend \
-   --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --fork
+# Shut down any running MongoDB instance
+mongod --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --shutdown || true
 
-# init replset with defaults
-mongo 10.7.7.6 --eval "rs.initiate({
-   _id: 'rs0',
-   members: [ { _id: 0, host: '10.7.7.6:27017' } ]
+# Restart MongoDB with replica set and appropriate bindings
+mongod --oplogSize 8 --replSet rs0 --noauth \
+  --bind_ip 0.0.0.0 --port 27017 \
+  --tlsMode disabled \
+  --logpath /proc/1/fd/1 --logappend \
+  --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --fork
+
+# Initialize replica set using the current hostname
+mongo "${host}" --eval "rs.initiate({
+  _id: 'rs0',
+  members: [ { _id: 0, host: '${host}:27017' } ]
 })"
 
 echo "Waiting to become a master"
-echo 'while (!db.isMaster().ismaster) { sleep(100); }' | mongo
+echo 'while (!db.isMaster().ismaster) { sleep(100); }' | mongo "${host}"
 
 echo "I'm the master!"
