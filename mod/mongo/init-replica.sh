81e27017a895996753529373771836d17eff38ff
Akpoguma <113827909+Akpoguma@users.noreply.github.com>
2024-08-27 14:55:33 +0200

    Update init-replica.sh

--- mod/mongo/init-replica.sh
+++ mod/mongo/init-replica.sh
@@ -5,15 +5,7 @@ set -e
 
 host=${HOSTNAME:-$(hostname -f)}
 
-# shut down again
-mongod --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --shutdown
-# restart again binding to 0.0.0.0 to allow a replset with 10.7.7.6
-mongod --oplogSize 8 --replSet rs0 --noauth \
-   --config /tmp/docker-entrypoint-temp-config.json \
-   --bind_ip 0.0.0.0 --port 27017 \
-   --tlsMode disabled \
-   --logpath /proc/1/fd/1 --logappend \
-   --pidfilepath /tmp/docker-entrypoint-temp-mongod.pid --fork
+
 
 
 # init replset with defaults
