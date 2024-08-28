288eeee76101828cdefc9b990fecc2ff09582f47
Akpoguma <113827909+Akpoguma@users.noreply.github.com>
2024-08-28 19:18:03 +0200

    test

--- mod/recordings/entrypoint.sh
+++ mod/recordings/entrypoint.sh
@@ -2,15 +2,10 @@
 
 touch /var/log/bigbluebutton/recording.log
 touch /var/log/bigbluebutton/bbb-web.log
-touch /var/log/bigbluebutton/sanity.log
-touch /var/log/bigbluebutton/post_publish.log
 mkdir -p /var/log/bigbluebutton/presentation
 chown -R bigbluebutton:bigbluebutton /var/log/bigbluebutton
 
 dockerize \
-    -template /etc/bigbluebutton/recording/recording.yml.tmpl:/etc/bigbluebutton/recording/recording.yml \
-    -template /etc/bigbluebutton/bbb-web.properties.tmpl:/etc/bigbluebutton/bbb-web.properties \
+    -template /usr/local/bigbluebutton/core/scripts/bigbluebutton.yml.tmpl:/usr/local/bigbluebutton/core/scripts/bigbluebutton.yml \
     -stdout /var/log/bigbluebutton/recording.log \
-    -stdout /var/log/bigbluebutton/post_publish.log \
-    -stdout /var/log/bigbluebutton/sanity.log \
     /usr/bin/supervisord --nodaemon
