8dc0c07e0bf7e1eb8ed1ac9f09a4e61252a0337c
Akpoguma <113827909+Akpoguma@users.noreply.github.com>
2024-08-28 12:56:37 +0200

    Update init-replica.sh

--- mod/mongo/init-replica.sh
+++ mod/mongo/init-replica.sh
@@ -9,7 +9,7 @@ host=${HOSTNAME:-$(hostname -f)}
 
 
 # init replset with defaults
-mongo 127.0.0.1 --eval "rs.initiate({
+mongo local --eval "rs.initiate({
    _id: 'rs0',
    members: [ { _id: 0, host: '127.0.0.1:27017' } ]
 })"
